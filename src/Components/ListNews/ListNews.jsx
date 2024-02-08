929fc9d45ac89e59eee7a1df2daf392a37032c2b
AdrikLarreategui <adrian15186@hotmail.com>
2024-02-08 14:01:01 +0100

    commit ListNews hecho. Falta la Api y los estilos

---
+++ src/Components/ListNews/ListNews.jsx
@@ -0,0 +1,37 @@
+import '/ListNews.styles.scss'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { axios } from 'axios'
+import Apikey from '../ApiKey/ApiKey'
+
+const ListNews = () => {
+    const [ articles, setArticles ] = useState([])
+
+    const getArticles = async () => {
+        try {
+            const response = await axios.get(`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${ApiKey.key}`)
+            setArticles(response.data.results)
+        } catch (error) {
+            console.error (error)
+        }
+    }
+
+    useEffect(() => {
+        getArticles()
+    }, [])
+
+    return(
+        <div>
+            <>
+            {articles.map((article, index) => (
+                <p key={index}>{article.title}
+                <p>{article.abstract}</p>
+                <Link to= {article.url}>{article.url}</Link>
+                </p>        
+            ))}
+            </>
+        </div>
+    )
+}
+
+export default ListNews
